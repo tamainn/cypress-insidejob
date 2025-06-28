6601bf1d66b02717053f133c7a203f411da53ead
Tamain <mrslptra@gmail.com>
2025-06-29 03:43:01 +0700

    Fix login_command import path and structure

--- cypress/e2e/login_command.cy.js
+++ cypress/e2e/login_command.cy.js
@@ -1,11 +1,17 @@
 import LoginPage from "../support/objectModel/LoginPage";
 
-
 describe('Fitur Login', () => {
   const loginPage = new LoginPage(); 
 
   it('User Berhasil Login', () => {
+    
     cy.visit('https://www.saucedemo.com/');
+
+    
     loginPage.login('standard_user', 'secret_sauce');
+
+   
+    cy.url().should('include', '/inventory.html');
+    cy.get('.title').should('contain', 'Products'); 
   });
 });
--- cypress/support/objectModel/LoginPage.js
+++ cypress/support/objectModel/LoginPage.js
@@ -1,26 +1,8 @@
-// cypress/support/objectModel/LoginPage.js
-
 class LoginPage {
-  visit() {
-    cy.visit('/login'); // ganti sesuai URL login-mu
-  }
-
-  fillUsername(username) {
-    cy.get('[data-test="username"]').type(username);
-  }
-
-  fillPassword(password) {
-    cy.get('[data-test="password"]').type(password);
-  }
-
-  submit() {
-    cy.get('[data-test="login-button"]').click();
-  }
-
   login(username, password) {
-    this.fillUsername(username);
-    this.fillPassword(password);
-    this.submit();
+    cy.get('[data-test="username"]').type(username);
+    cy.get('[data-test="password"]').type(password);
+    cy.get('[data-test="login-button"]').click();
   }
 }
 
